29359b358085bc56dcd59347cb1b171ac3edc612
togatoga <togasakitogatoga@gmail.com>
2017-12-10 10:57:19 +0900

    Add newDocument to codeforces.go

--- problem/codeforces.go
+++ problem/codeforces.go
@@ -1,8 +1,24 @@
 package problem
 
+import (
+	"github.com/PuerkitoBio/goquery"
+)
+
 type Codeforces struct {
+	URL string
+	Doc *goquery.Document
+}
+
+func (c *Codeforces) newDocument() error {
+	doc, err := goquery.NewDocument(c.URL)
+	if err != nil {
+		return err
+	}
+	c.Doc = doc
+	return nil
 }
 
 func (c *Codeforces) GetProblemName() (string, error) {
+
 	return "", nil
 }
