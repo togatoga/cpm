9f3f28cb9620641cbbc113e2d5df976aad2009b4
togatoga <togasakitogatoga@gmail.com>
2017-12-22 07:36:16 +0900

    Update description

--- cmd/get.go
+++ cmd/get.go
@@ -29,13 +29,10 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "Create problem directory",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create problem directory by URL",
+	Long: `Get problem information by URL.
+Create structural problem directory under cpm root
+Currently cpm supports Codeforces, AtCoder.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		values := map[string]bool{}
 		for i := 0; i < len(args); i++ {
