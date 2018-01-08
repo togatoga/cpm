04769722d68a6752c96eee30b7fda954e5b025a2
togatoga <togasakitogatoga@gmail.com>
2018-01-08 19:57:21 +0900

    Update description about cmd/run.go

--- cmd/run.go
+++ cmd/run.go
@@ -32,14 +32,9 @@ import (
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: run,
+	Short: "Run Test",
+	Long:  `Run Test to exec command.`,
+	Run:   run,
 }
 
 func run(cmd *cobra.Command, args []string) {
