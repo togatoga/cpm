b86de1da68b4ac004774ea0cf030e3f6030ad250
togatoga <togasakitogatoga@gmail.com>
2020-02-18 20:45:30 +0900

    Fix compile errors

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,13 @@ impl SubCommand {
 struct Config {
     root: String,
 }
-
+fn load_config() -> Result<Config, failure::Error> {
+    let config_file = dirs::config_dir().unwrap().join("cpm").join("config.json");
+    let file = std::fs::File::open(config_file)?;
+    let reader = std::io::BufReader::new(file);
+    let config: Config = serde_json::from_reader(reader)?;
+    Ok(config)
+}
 struct AtCoder {
     client: reqwest::Client,
     //for request
@@ -155,7 +161,7 @@ impl AtCoder {
         problem_name.retain(|x| !x.is_whitespace());
         contest_name.retain(|x| !x.is_whitespace());
         println!("{} {}", contest_name, problem_name);
-        let config = AtCoder::load_config()?;
+        let config = load_config()?;
         let path = std::path::PathBuf::from(config.root)
             .join("atcoder.jp")
             .join(contest_name)
@@ -253,90 +259,6 @@ impl AtCoder {
         self.cookie_headers = cookie_headers;
         self.html = Some(response.text().await?);
         Ok(())
-    }
-
-    //utils
-    fn load_config() -> Result<Config, failure::Error> {
-        let config_file = dirs::config_dir().unwrap().join("cpm").join("config.json");
-        let file = std::fs::File::open(config_file)?;
-        let reader = std::io::BufReader::new(file);
-        let config: Config = serde_json::from_reader(reader)?;
-        Ok(config)
-    }
-    fn create_problem_directory(
-        contest_name: &str,
-        problem_name: &str,
-    ) -> Result<(), failure::Error> {
-        Ok(())
-    }
-    fn create_sample_test_files(
-        test_cases: &[(String, String)],
-        path: Option<&str>,
-    ) -> Result<(), failure::Error> {
-        let root_path = if let Some(p) = path {
-            std::path::PathBuf::from(p)
-        } else {
-            dirs::runtime_dir().unwrap()
-        };
-        std::fs::create_dir_all(root_path.clone())?;
-        for (idx, (input, output)) in test_cases.iter().enumerate() {
-            //e.g sample_input_1.txt sample_output_1.txt
-            let input_file_name = format!("sample_input_{}.txt", idx + 1);
-            let mut input_file = std::fs::OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(root_path.join(input_file_name))?;
-            input_file.write_all(input.as_bytes())?;
-
-            let output_file_name = format!("sample_output_{}.txt", idx + 1);
-            let mut output_file = std::fs::OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(root_path.join(output_file_name))?;
-            output_file.write_all(output.as_bytes())?;
-        }
-        Ok(())
-    }
-    fn save_cookie_in_local(response: &reqwest::Response) -> Result<(), failure::Error> {
-        let cookies_str = response
-            .cookies()
-            .map(|cookie| format!("{}={}", cookie.name(), cookie.value()))
-            .join(";");
-        let path = dirs::home_dir().unwrap().join(".atcoder-sample-downloader");
-        //create $HOME/.atcoder-sample-downloader
-        std::fs::create_dir_all(path.clone())?;
-        //create cookie.jar under this directory
-        let cookie_path = path.join("cookie.jar");
-        std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(cookie_path.clone())?
-            .write_all(cookies_str.as_bytes())?;
-        println!("SAVED YOUR COOKIE IN {}", cookie_path.to_str().unwrap());
-        Ok(())
-    }
-    fn username_and_password() -> (String, String) {
-        println!("Please input Your username and password");
-        let username = rpassword::read_password_from_tty(Some("Username > ")).unwrap();
-        let password = rpassword::read_password_from_tty(Some("Password > ")).unwrap();
-        (username, password)
-    }
-    fn local_cookie_headers() -> Result<HeaderMap, failure::Error> {
-        let cookiejar_path = dirs::home_dir()
-            .unwrap()
-            .join(".atcoder-sample-downloader")
-            .join("cookie.jar");
-        let file = std::fs::File::open(cookiejar_path)?;
-        let reader = std::io::BufReader::new(file);
-
-        let mut cookie_headers = HeaderMap::new();
-        reader.lines().for_each(|line| {
-            cookie_headers.insert(
-                COOKIE,
-                HeaderValue::from_str(&format!("{}", line.unwrap())).unwrap(),
-            );
-        });
-        Ok(cookie_headers)
     }
 }
 
--- src/util.rs
+++ src/util.rs
@@ -2,21 +2,36 @@ use itertools::Itertools;
 use reqwest::header::{HeaderMap, HeaderValue, COOKIE};
 use std::io::{BufRead, Write};
 
-pub fn create_sample_test_files(test_cases: &[(String, String)]) -> Result<(), failure::Error> {
+pub fn create_problem_directory(
+    contest_name: &str,
+    problem_name: &str,
+) -> Result<(), failure::Error> {
+    Ok(())
+}
+pub fn create_sample_test_files(
+    test_cases: &[(String, String)],
+    path: Option<&str>,
+) -> Result<(), failure::Error> {
+    let root_path = if let Some(p) = path {
+        std::path::PathBuf::from(p)
+    } else {
+        dirs::runtime_dir().unwrap()
+    };
+    std::fs::create_dir_all(root_path.clone())?;
     for (idx, (input, output)) in test_cases.iter().enumerate() {
         //e.g sample_input_1.txt sample_output_1.txt
         let input_file_name = format!("sample_input_{}.txt", idx + 1);
         let mut input_file = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
-            .open(input_file_name)?;
+            .open(root_path.join(input_file_name))?;
         input_file.write_all(input.as_bytes())?;
 
         let output_file_name = format!("sample_output_{}.txt", idx + 1);
         let mut output_file = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
-            .open(output_file_name)?;
+            .open(root_path.join(output_file_name))?;
         output_file.write_all(output.as_bytes())?;
     }
     Ok(())
