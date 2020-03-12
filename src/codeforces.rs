1da8f7f3570e806d26da1f243c048a3e7db87f9a
togatoga <togasakitogatoga@gmail.com>
2020-03-12 12:48:55 +0900

    Update

--- src/codeforces.rs
+++ src/codeforces.rs
@@ -1,3 +1,4 @@
+use crate::parser::Parser;
 pub struct CodeforcesParser {
     document: scraper::Html,
 }
@@ -7,5 +8,17 @@ impl CodeforcesParser {
         CodeforcesParser {
             document: scraper::Html::parse_document(html),
         }
+    }
+}
+
+impl Parser for CodeforcesParser {
+    fn problem_name(&self) -> Option<String> {
+        None
+    }
+    fn contest_name(&self) -> Option<String> {
+        None
+    }
+    fn sample_cases(&self) -> Option<Vec<(String, String)>> {
+        None
     }
 }
